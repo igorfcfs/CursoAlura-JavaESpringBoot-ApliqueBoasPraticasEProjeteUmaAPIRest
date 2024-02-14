3965b215a0cc440630b5ef51e4e26d3f4d2f4c8c
igorfcfs <igorfcfs@gmail.com>
2024-02-14 16:13:55 -0300

    Controle de Acesso

--- src/main/java/med/voll/api/infra/security/TokenService.java
+++ src/main/java/med/voll/api/infra/security/TokenService.java
@@ -1,7 +1,6 @@
 package med.voll.api.infra.security;
 
 import java.time.Instant;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 
@@ -11,6 +10,7 @@ import org.springframework.stereotype.Service;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 
 import med.voll.api.domain.usuario.Usuario;
 
@@ -19,18 +19,24 @@ public class TokenService {
 	
 	@Value("${api.security.token.secret}")
 	private String secret;
-	
+
 	public String gerarToken(Usuario usuario) {
 		try {
 			System.out.println(secret);
-		    var algoritmo = Algorithm.HMAC256(secret);
-		    return JWT.create()
-		        .withIssuer("API Voll.med")
-		        .withSubject(usuario.getLogin())
-		        .withExpiresAt(dataExpiracao())
-		        .sign(algoritmo);
-		} catch (JWTCreationException exception){
+			var algoritmo = Algorithm.HMAC256(secret);
+			return JWT.create().withIssuer("API Voll.med").withSubject(usuario.getLogin())
+					.withExpiresAt(dataExpiracao()).sign(algoritmo);
+		} catch (JWTCreationException exception) {
 			throw new RuntimeException("Erro ao gerar token de acesso jwt", exception);
+		}
+	}
+
+	public String getSubject(String tokenJWT) {
+		try {
+			var algoritmo = Algorithm.HMAC256(secret);
+			return JWT.require(algoritmo).withIssuer("API Voll.med").build().verify(tokenJWT).getSubject();
+		} catch (JWTVerificationException exception) {
+			throw new RuntimeException("Token JWT inválido ou expirado!");
 		}
 	}
 
